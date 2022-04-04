6934d40ed2cec74b25ddd22aa9c881ff51cd8b7a
unknown <samara_alves28@hotmail.com>
2022-04-04 18:32:28 -0300

    Task: Ataualizações dos Getters e Setters

--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -45,49 +45,54 @@ public class Postagem {
     @ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Usuario usuario;
-  	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
-	
 	}
-	public void setDate(LocalDate data) {
-		this.data = data;
-	}
-	
-  	public Tema getTema() {
-		return tema;
-	}
-	public void setTema(Tema tema) {
-		this.tema = tema;
-	
-	}
+
 	public LocalDate getData() {
 		return data;
 	}
+
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+  	
 	
-
 }
